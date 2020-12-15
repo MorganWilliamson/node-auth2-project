aaa7c9e795e8f1d2e3cec5048e7c856abb9028a4
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-15 17:25:50 -0600

    Fleshed out endpoints, testing.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const router = require('express').Router();
-
 const User = require('../users/users-model');
 const { isValid } = require('../users/users-service');
+const { makeToken } = require('../middleware/make-token');
 
 router.post('/register', (req, res) => {
     const creds = req.body;
@@ -26,3 +25,31 @@ router.post('/register', (req, res) => {
     }
 });
 
+router.post('login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (isValid(req.body)) {
+        User.findBy({ username: username })
+        .then(([user]) => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = makeToken(user);
+
+                res.status(200).json({
+                    message: "Welcome to the API, " + user.username,
+                    token
+                });
+            } else {
+                res.status(401).json({ message: "Invalid credentials." });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+    } else {
+        res.status(400).json({
+            message: "Please provide a valid username and password."
+        });
+    }
+});
+
+module.exports = router;
