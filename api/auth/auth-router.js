929c8462a42f8536823c1645895efe53a5bf2b8c
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-15 17:06:30 -0600

    Finished middleware, started user-model, started auth endpoints.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,3 +1,28 @@
-module.exports = {
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const router = require('express').Router();
 
-};
+const User = require('../users/users-model');
+const { isValid } = require('../users/users-service');
+
+router.post('/register', (req, res) => {
+    const creds = req.body;
+
+    if (isValid(creds)) {
+        const rounds = process.env.BCRYPT_ROUNDS || 10;
+
+        const hash = bcrypt.hashSync(creds.password, rounds);
+        creds.password = hash;
+
+        User.add(creds)
+            .then((user) => {
+                res.status(201).json({ data: user });
+            })
+            .catch((err) => {
+                res.status(500).json({ message: err.message });
+            });
+    } else {
+        res.status(400).json({ message: "Provide a username and alphanumeric password." });
+    }
+});
+
