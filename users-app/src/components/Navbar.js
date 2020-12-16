c1aec24250fe846121c2b492b28c04cc89eebe60
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-15 21:14:47 -0600

    Quick styling for navbar.

--- users-app/src/components/Navbar.js
+++ users-app/src/components/Navbar.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import "../stylesheets/Navbar.css";
 
 function Navbar() {
-
     return (
         <Router>
-            <nav>
-                <div>
+            <nav className="navbar">
+                <div className="unlist">
                     <ul>
-                    <p>Navbar rendering here.</p>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">Register</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/users">Users</Link>
-                    </li>
+                        <p>Navbar rendering here.</p>
+                        <li>
+                            <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/register" style={{ textDecoration: 'none' }}>Register</Link>
+                        </li>
+                        <li>
+                            <Link to="/login" style={{ textDecoration: 'none' }}>Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/users" style={{ textDecoration: 'none' }}>Users</Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
